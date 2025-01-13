91ed40dd83b87cd4ebd9410c7039d97a46750c31
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-01-13 13:57:02 +0100

    print_arguments

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python3
-
 import sys
 
-
-def print_arguments(args):
-    """
-    Affiche les arguments passés au script.
-
-    :param args: Liste des arguments, y compris le nom du script.
-    """
-    # Vérifie si des arguments ont été passés
-    if len(args) == 1:
-        print("Aucun argument fourni.")
-    else:
-        # Itère sur les arguments (en excluant le nom du script)
-        for arg in args[1:]:
-            print(arg)
-
-
-if __name__ == "__main__":
-    # Appelle la fonction avec les arguments du script
-    print_arguments(sys.argv)
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
